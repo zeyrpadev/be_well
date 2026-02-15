5471c6705b7c8bf41057531b965bdc481ab246a3
zeyrpadev <zeyrpadevv@gmail.com>
2026-02-16 05:39:14 +0800

    Update app.py

--- app.py
+++ app.py
@@ -523,7 +523,7 @@ def login_screen():
 
     st.markdown(
         """
-        <div class="social-btn">
+        <div class="social-row">
             <img src="https://img.icons8.com/?size=100&id=30840&format=png&color=000000"
                      width="20" style="vertical-align:middle;">
             <div class="social-btn">
