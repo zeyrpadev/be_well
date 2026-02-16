0cc99457d0aa41814c16f9faac541d080dbca165
zeyrpadev <zeyrpadevv@gmail.com>
2026-02-17 04:29:19 +0800

    Update app.py

--- app.py
+++ app.py
@@ -785,18 +785,10 @@ def home_screen():
         st.markdown("<div class='carer-home-title'>Carer Home</div>", unsafe_allow_html=True)
 
     # New Case button (only for non-parents)
-    left, center, right = st.columns([1, 2, 1])
-    with center:
-
-        if role == "parent":
-            st.markdown("<div class='carer-home-title'>Parent Home</div>", unsafe_allow_html=True)
-        else:
-            st.markdown("<div class='carer-home-title'>Carer Home</div>", unsafe_allow_html=True)
-
-        if role != "parent":
-            if st.button("New Case  ➕", type="primary"):
-                navigate("symptom_entry")
-                st.rerun()
+    if role != "parent":
+        if st.button("New Case  ➕", type="primary"):
+            navigate("symptom_entry")
+            st.rerun()
 
     st.markdown("<div style='height:0.75rem;'></div>", unsafe_allow_html=True)
 
